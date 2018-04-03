fcf786a409520683cf0d435427ce4df4b232f9d4
andcrow25 <andcrow25@gmail.com>
2018-04-03 17:04:19 -0300

    Login manual

--- src/app/security/login/login.service.ts
+++ src/app/security/login/login.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/filter';
 
 import { MEAT_API } from '../../app.api';
 import { User } from './user.model'
@@ -15,9 +16,13 @@ export class LoginService {
 
 
   user: User;
+  lastUrl: string;
 
   constructor(private http: HttpClient,
-              private router: Router) {}
+              private router: Router) {
+    this.router.events.filter(e => e instanceof NavigationEnd)
+                      .subscribe( (e: NavigationEnd) => this.lastUrl = e.url);
+  }
 
   isLoggedIn(): boolean {
     return this.user !== undefined;
@@ -29,7 +34,11 @@ export class LoginService {
                     .do(user => this.user = user);
   }
 
-  handleLogin(path?: string) {
+  logout() {
+    this.user = undefined;
+  }
+
+  handleLogin(path: string = this.lastUrl) {
     this.router.navigate(['/login', btoa(path)]);
   }
 }
